244a197b2d3768705c4cf567be2ab5d3933af90b
Alexander Melo <alexandermelox@gmail.com>
2021-02-27 23:02:31 -0500

    Create useStickyValue.js

---
+++ useStickyValue.js
@@ -0,0 +1,13 @@
+// Used if you want to persist the state from ls and update when ls changes
+function useStickyState(defaultValue, key) {
+  const [value, setValue] = React.useState(() => {
+    const stickyValue = window.localStorage.getItem(key);
+    return stickyValue !== null
+      ? JSON.parse(stickyValue)
+      : defaultValue;
+  });
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}
